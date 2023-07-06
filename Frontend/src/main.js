ca6373cd27f09491cf70f0865464336fc6e67dff
Андрій Ачілов <andrry1@gmail.com>
2023-07-06 22:18:13 +0300

    Clear default content; install sass

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -1,6 +1,7 @@
-import './assets/main.css'
+import { createApp } from "vue";
+import App from "./App.vue";
 
-import { createApp } from 'vue'
-import App from './App.vue'
+import "./assets/styles.scss";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
 
-createApp(App).mount('#app')
+createApp(App).mount("#app");
