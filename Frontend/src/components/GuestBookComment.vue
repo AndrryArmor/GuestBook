bac47654a39f6cff597975ce0f97e9519b977ed9
Андрій Ачілов <andrry1@gmail.com>
2023-07-09 19:57:47 +0300

    Change predefined comment data to props

--- Frontend/src/components/GuestBookComment.vue
+++ Frontend/src/components/GuestBookComment.vue
@@ -1,21 +1,28 @@
 <template>
   <div class="comment rounded-3 col-xxl-10 p-2">
-    <span class="h6">Author </span>
-    <small>15:56 03.05.19</small>
+    <span class="h6">{{ authorName + " " }}</span>
+    <small>{{ publicationDateString }}</small>
     <br />
-    <span>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-      labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-      laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-      voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
-      non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-    </span>
+    <span> {{ comment }} </span>
   </div>
 </template>
 
 <script>
 export default {
   name: "GuestBookComment",
+  props: {
+    authorName: String,
+    publicationDate: Date,
+    comment: String,
+  },
+
+  computed: {
+    publicationDateString() {
+      let time = this.publicationDate.toLocaleTimeString([], {timeStyle: "short"});
+      let date = this.publicationDate.toLocaleDateString();
+      return `${time} ${date}`;
+    },
+  },
 };
 </script>
 
