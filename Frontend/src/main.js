5f756d99f5b69de7f3ea03680e44a8c1f759924e
Андрій Ачілов <andrry1@gmail.com>
2023-07-22 18:25:23 +0300

    Remove bootstrap package

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -1,7 +1,4 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 
-import "./assets/styles.scss";
-import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
-
 createApp(App).mount("#app");
